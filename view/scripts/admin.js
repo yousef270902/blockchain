c5c6ae25eee5f1a4b1155899aeee3ab604d28580
AbdurRahman <97107934+AbdurRahman-Ossama@users.noreply.github.com>
2024-05-01 19:55:43 +0000

    git add .

---
+++ view/scripts/admin.js
@@ -0,0 +1,23 @@
+ 
+        function campaign()
+        {
+let x = document.getElementById("table_campaign");
+let y = document.getElementById("table_user");
+if( x.style.display=="none")
+{
+  x.style.display="block";
+  y.style.display="none";
+}
+        }
+        function user()
+        {
+let x = document.getElementById("table_campaign");
+let y = document.getElementById("table_user");
+if( y.style.display=="none")
+{
+  x.style.display="none";
+  y.style.display="block";
+}
+        }
+
+       
